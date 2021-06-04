4a5af6525e7a73a55fd33adeef18e27e7daea6f7
Augis08 <73080958+Augis08@users.noreply.github.com>
2021-06-05 01:50:52 +0300

    ProductService and ProductController

---
+++ src/main/java/com/example/onlinestore/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.example.onlinestore.controller;
+
+import com.example.onlinestore.model.entity.Product;
+import com.example.onlinestore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping(value = "/{productId}")
+    public Optional<Product> getProductById (@PathVariable Long productId){
+        return productService.getProductById(productId);
+    }
+}
